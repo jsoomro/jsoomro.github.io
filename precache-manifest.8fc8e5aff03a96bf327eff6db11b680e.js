994c3ba081dbd7921a627267b0faf9ed66044d71
ajsoomro <jalilsoomro@gmail.com>
2020-06-24 13:16:48 -0500

    react project init

---
+++ precache-manifest.8fc8e5aff03a96bf327eff6db11b680e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27265f886b71153c9d69478938c8330a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "614089cea540255012b5",
+    "url": "/static/css/main.83f2ebc2.chunk.css"
+  },
+  {
+    "revision": "6d649dc1ff4678708901",
+    "url": "/static/js/2.ca7e587a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca7e587a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614089cea540255012b5",
+    "url": "/static/js/main.e4a7d6c9.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
